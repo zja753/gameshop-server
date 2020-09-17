7f3cc7ed7e834ad77bf9dd9c1f8e12b255316fa0
gkmg <2319454362@qq.com>
2020-09-17 14:35:05 +0800

    功能优化

--- routes/tag.js
+++ routes/tag.js
@@ -1,165 +1,182 @@
 const router = require('koa-router')()
+const ObjectId = require('mongodb').ObjectId;
 
 router.prefix('/tag')
 
 router.post('/create', async function (ctx, next) {
-    const {
-        name = null, introduction = ""
-    } = await ctx.request.body;
-    if (name === null) {
-        ctx.body = {
-            status: 0,
-            err: '标签名字不能为空',
-            data: {}
-        }
-    } else {
-        try {
-            let tag = await DB.findOne('tag', {
-                name,
-                status: 1
-            })
-            if (!tag) {
-                tag = await DB.insert('tag', {
-                    name,
-                    introduction
-                })
-                ctx.body = {
-                    status: 1,
-                    msg: '创建标签成功',
-                    data: tag
-                }
-            } else {
-                ctx.body = {
-                    status: 0,
-                    err: '标签已存在',
-                    data: tag
-                }
-            }
-        } catch (err) {
-            ctx.body = {
-                status: 0,
-                err: '创建标签失败',
-                data: err
-            }
-        }
+  const {
+    name = null, introduction = ""
+  } = await ctx.request.body;
+  if (name === null) {
+    ctx.body = {
+      status: 0,
+      err: '标签名字不能为空',
+      data: {}
     }
+  } else {
+    try {
+      let tag = await DB.findOne('tag', {
+        name,
+        status: 1
+      })
+      if (!tag) {
+        tag = await DB.insert('tag', {
+          name,
+          introduction
+        })
+        ctx.body = {
+          status: 1,
+          msg: '创建标签成功',
+          data: tag
+        }
+      } else {
+        ctx.body = {
+          status: 0,
+          err: '标签已存在',
+          data: tag
+        }
+      }
+    } catch (err) {
+      ctx.body = {
+        status: 0,
+        err: '创建标签失败',
+        data: err
+      }
+    }
+  }
 })
 
 router.post('/update', async function (ctx, next) {
-    const {
-        _id = null, introduction = null
-    } = await ctx.request.body;
-    if (_id === null) {
-        ctx.body = {
-            status: 0,
-            err: '未传入_id',
-            data: {}
-        }
-    } else {
-        const updateObj = {}
-        if (introduction !== null) updateObj.introduction = introduction;
-        try {
-            await DB.update('tag', {
-                _id,
-                status: 1
-            }, updateObj)
-            ctx.body = {
-                status: 1,
-                msg: '更新标签描述成功',
-                data: {}
-            }
-        } catch (err) {
-            ctx.body = {
-                status: 0,
-                err: '更新标签失败',
-                data: err
-            }
-        }
+  const {
+    _id = null, introduction = null, name = null
+  } = await ctx.request.body;
+  if (_id === null) {
+    ctx.body = {
+      status: 0,
+      err: '未传入_id',
+      data: {}
     }
+  } else {
+    const updateObj = {}
+    if (introduction !== null && name !== null) {
+      updateObj.introduction = introduction;
+      updateObj.name = name;
+    }
+    try {
+      let tag = await DB.findOne('tag', {
+        name,
+        status: 1
+      })
+      if (!tag) {
+        await DB.update('tag', {
+          _id: ObjectId(_id),
+          status: 1
+        }, updateObj)
+        ctx.body = {
+          status: 1,
+          msg: '更新标签描述成功',
+          data: {}
+        }
+      } else {
+        ctx.body = {
+          status: 0,
+          err: '标签已存在',
+          data: tag
+        }
+      }
+
+    } catch (err) {
+      ctx.body = {
+        status: 0,
+        err: '更新标签失败',
+        data: err
+      }
+    }
+  }
 })
 
 router.get('/fetch', async function (ctx, next) {
-    const {
-        page = 0, limit = 10
-    } = ctx.query
-    try {
-        const tagList = await DB.pagination('tag', {
-            status: 1
-        }, page, limit)
-        ctx.body = {
-            status: 1,
-            msg: '获取标签列表成功',
-            data: tagList
-        }
-    } catch (err) {
-        ctx.body = {
-            status: 0,
-            err: "获取标签列表失败",
-            data: err
-        }
+  const {
+    page = 0, limit = 10
+  } = ctx.query
+  try {
+    const tagList = await DB.pagination('tag', {
+      status: 1
+    }, page, limit)
+    ctx.body = {
+      status: 1,
+      msg: '获取标签列表成功',
+      data: tagList
     }
+  } catch (err) {
+    ctx.body = {
+      status: 0,
+      err: "获取标签列表失败",
+      data: err
+    }
+  }
 })
 
 router.get('/get', async function (ctx, next) {
-    const {
-        _id = null
-    } = ctx.query
-    if (_id === null) {
-        ctx.body = {
-            status: 0,
-            err: "未传入_id",
-            data: {}
-        }
-    } else {
-        try {
-            const tag = await DB.findOne('tag', {
-                _id
-            })
-            ctx.body = {
-                status: 1,
-                msg: '获取标签成功',
-                data: tag
-            }
-        } catch (err) {
-            ctx.body = {
-                status: 0,
-                err: "获取标签失败",
-                data: err
-            }
-        }
+  const {
+    _id = null
+  } = ctx.query
+  if (_id === null) {
+    ctx.body = {
+      status: 0,
+      err: "未传入_id",
+      data: {}
     }
+  } else {
+    try {
+      const tag = await DB.findOne('tag', {
+        _id: ObjectId(_id),
+      })
+      ctx.body = {
+        status: 1,
+        msg: '获取标签成功',
+        data: tag
+      }
+    } catch (err) {
+      ctx.body = {
+        status: 0,
+        err: "获取标签失败",
+        data: err
+      }
+    }
+  }
 })
 
 router.post('/delete', async function (ctx, next) {
-    const {
-        _id = null
-    } = await ctx.request.body
-    if (_id === null) {
-        ctx.body = {
-            status: 0,
-            err: "未传入_id",
-            data: {}
-        }
-    } else {
-        try {
-            await DB.update('tag', {
-                _id,
-            }, {
-                status: 0
-            })
-            ctx.body = {
-                status: 1,
-                msg: '删除标签成功',
-                data: {}
-            }
-        } catch (err) {
-            ctx.body = {
-                status: 0,
-                err: '删除标签失败',
-                data: err
-            }
-        }
+  const {
+    _id = null
+  } = await ctx.request.body
+  if (_id === null) {
+    ctx.body = {
+      status: 0,
+      err: "未传入_id",
+      data: {}
     }
+  } else {
+    try {
+      await DB.update('tag', {
+        _id: ObjectId(_id),
+      }, {
+        status: 0
+      })
+      ctx.body = {
+        status: 1,
+        msg: '删除标签成功',
+        data: {}
+      }
+    } catch (err) {
+      ctx.body = {
+        status: 0,
+        err: '删除标签失败',
+        data: err
+      }
+    }
+  }
 })
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -6,178 +6,180 @@ const ObjectId = require('mongodb').ObjectId;
 router.prefix("/user");
 
 router.post("/register", async (ctx) => {
-    const {
-        password,
-        email,
-        nickName = "admin"
-    } = await ctx.request.body;
-    console.log({
-        password,
-        email,
-        nickName
+  const {
+    password,
+    email,
+    nickName = "admin"
+  } = await ctx.request.body;
+  console.log(ctx.request);
+  console.log({
+    password,
+    email,
+    nickName
+  });
+  if (password.length >= 6) {
+    const user = await DB.findOne("user", {
+      email,
+      status: 1
     });
-    if (password.length >= 6) {
-        const user = await DB.findOne("user", {
-            email,
-            status: 1
-        });
-        if (user === null) {
-            const hashPassword = bcrypt.hashSync(password);
-            const res = await DB.insert("user", {
-                password: hashPassword,
-                email,
-                nickName,
-                balance: 0,
-                avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600104621985&di=48f4ff75babdb2c0652913f5ff201955&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201603%2F06%2F20160306204517_i4Se8.jpeg"
-            });
-            ctx.body = {
-                status: 1,
-                err: "账号注册成功",
-            };
-        } else {
-            ctx.body = {
-                status: 0,
-                err: "账号已存在",
-            };
-        }
+    if (user === null) {
+      const hashPassword = bcrypt.hashSync(password);
+      const res = await DB.insert("user", {
+        password: hashPassword,
+        email,
+        nickName,
+        balance: 0,
+        avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600104621985&di=48f4ff75babdb2c0652913f5ff201955&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201603%2F06%2F20160306204517_i4Se8.jpeg"
+      });
+      ctx.body = {
+        status: 1,
+        data: res,
+        err: "账号注册成功",
+      };
     } else {
-        ctx.body = {
-            status: 0,
-            err: "密码长度小于6位"
-        }
+      ctx.body = {
+        status: 0,
+        err: "账号已存在",
+      };
     }
+  } else {
+    ctx.body = {
+      status: 0,
+      err: "密码长度小于6位"
+    }
+  }
 });
 
 router.post("/login", async (ctx) => {
-    const {
-        email,
-        password
-    } = ctx.request.body;
-    console.log({
-        email,
-        password
-    });
-    const user = await DB.findOne("user", {
-        email,
-        status: 1
-    });
-    if (user === null) {
-        ctx.body = {
-            status: 0,
-            err: "账号不存在"
-        };
-    } else {
-        const confirmRes = bcrypt.compareSync(password, user.password);
-        if (confirmRes) {
-            console.log(user);
-            const token = jwt.sign({
-                    _id: user._id,
-                    email: user.email
-                },
-                "secret", {
-                    expiresIn: 3600 * 24
-                }
-            );
-            ctx.body = {
-                data: {
-                    token: `Bearer ${token}`,
-                    user_id: user._id,
-                    email,
-                },
-                msg: "登录成功",
-                status: 1,
-            };
-        } else ctx.body = {
-            status: 0,
-            err: "密码错误"
-        };
-    }
+  const {
+    email,
+    password
+  } = ctx.request.body;
+  console.log({
+    email,
+    password
+  });
+  const user = await DB.findOne("user", {
+    email,
+    status: 1
+  });
+  if (user === null) {
+    ctx.body = {
+      status: 0,
+      err: "账号不存在"
+    };
+  } else {
+    const confirmRes = bcrypt.compareSync(password, user.password);
+    if (confirmRes) {
+      console.log(user);
+      const token = jwt.sign({
+          _id: user._id,
+          email: user.email
+        },
+        "secret", {
+          expiresIn: 3600 * 24
+        }
+      );
+      ctx.body = {
+        data: {
+          token: `Bearer ${token}`,
+          user_id: user._id,
+          email,
+        },
+        msg: "登录成功",
+        status: 1,
+      };
+    } else ctx.body = {
+      status: 0,
+      err: "密码错误"
+    };
+  }
 });
 
 router.get("/getUserInfo", async (ctx) => {
-    console.log(1234);
-    const {
-        _id
-    } = ctx.query;
-    const user = await DB.findOne("user", {
-        _id: ObjectId(_id),
-        status: 1
-    });
+  console.log(1234);
+  const {
+    _id
+  } = ctx.query;
+  const user = await DB.findOne("user", {
+    _id: ObjectId(_id),
+    status: 1
+  });
 
-    if (user === null) {
-        ctx.body = {
-            status: 0,
-            err: "账号不存在"
-        };
-    } else {
-        ctx.body = {
-            data: {
-                nickName: user.nickName,
-                email: user.email,
-                avatar: user.avatar,
-                balance: user.balance,
-            },
-            status: 1,
-            msg: "请求成功",
-        };
-    }
+  if (user === null) {
+    ctx.body = {
+      status: 0,
+      err: "账号不存在"
+    };
+  } else {
+    ctx.body = {
+      data: {
+        nickName: user.nickName,
+        email: user.email,
+        avatar: user.avatar,
+        balance: user.balance,
+      },
+      status: 1,
+      msg: "请求成功",
+    };
+  }
 });
 
 router.get("/fetch", async (ctx, next) => {
-    const {
-        page,
-        limit
-    } = ctx.query;
-    try {
-        const userList = await DB.pagination('user', {
-            status: 1
-        }, page, limit);
-        ctx.body = {
-            status: 1,
-            msg: '获取用户列表成功',
-            data: userList
-        }
-    } catch (error) {
-        ctx.body = {
-            status: 0,
-            err: "获取标签列表失败",
-            data: error
-        }
+  const {
+    page,
+    limit
+  } = ctx.query;
+  try {
+    const userList = await DB.pagination('user', {
+      status: 1
+    }, page, limit);
+    ctx.body = {
+      status: 1,
+      msg: '获取用户列表成功',
+      data: userList
     }
+  } catch (error) {
+    ctx.body = {
+      status: 0,
+      err: "获取标签列表失败",
+      data: error
+    }
+  }
 })
 
 router.post('/delete', async ctx => {
-    let {
-        _id
-    } = await ctx.request.body;
+  let {
+    _id
+  } = await ctx.request.body;
 
-    if (_id === null) {
-        ctx.body = {
-            status: 0,
-            err: "未传入_id",
-            data: {}
-        }
-    } else {
-        try {
-            console.log(_id);
-            await DB.update('user', {
-                _id: ObjectId(_id)
-            }, {
-                status: 0
-            })
-            ctx.body = {
-                status: 1,
-                msg: '删除用户成功',
-                data: {}
-            }
-        } catch (err) {
-            ctx.body = {
-                status: 0,
-                err: '删除用户失败',
-                data: err
-            }
-        }
+  if (_id === null) {
+    ctx.body = {
+      status: 0,
+      err: "未传入_id",
+      data: {}
     }
+  } else {
+    try {
+      console.log(_id);
+      await DB.update('user', {
+        _id: ObjectId(_id)
+      }, {
+        status: 0
+      })
+      ctx.body = {
+        status: 1,
+        msg: '删除用户成功',
+        data: {}
+      }
+    } catch (err) {
+      ctx.body = {
+        status: 0,
+        err: '删除用户失败',
+        data: err
+      }
+    }
+  }
 
 })
 
